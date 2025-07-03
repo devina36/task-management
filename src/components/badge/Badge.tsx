c6994e2ce2da2465cb89dce26422e58f43aa858b
Devina Amalia <devina.amalia36@gmail.com>
2025-07-03 18:03:23 +0700

    badge icon

--- src/components/badge/Badge.tsx
+++ src/components/badge/Badge.tsx
@@ -3,11 +3,11 @@ import { ChevronDown, Equal, ChevronUp } from 'lucide-react';
 const getIcon = (priority: string) => {
   switch (priority) {
     case 'high':
-      return <ChevronDown size={20} className="text-red-400" />;
+      return <ChevronUp size={20} className="text-red-400" />;
     case 'medium':
       return <Equal size={20} className="text-yellow-400" />;
     case 'low':
-      return <ChevronUp size={20} className="text-blue-400" />;
+      return <ChevronDown size={20} className="text-blue-400" />;
     default:
       return <Equal size={20} className="text-gray-400" />;
   }
